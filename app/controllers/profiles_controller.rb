19d3d96040ca79faf9063515f34f0d30d190a9b9
Allan Lawrence <lawrenca@usa.net>
2016-03-16 02:56:50 +0000

    added filters and update action to single form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,20 +1,36 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
     def new
         @user = User.find( params[:user_id])
         @profile = Profile.new
     end
     def create 
-    @user = User.find( params[:user_id] )
-    @profile = @user.build_profile(profile_params)
-    if @profile.save
-      flash[:success] = "Profile Updated!"
-      redirect_to user_path( params[:user_id] )
-    else
-      render action: :new
+      @user = User.find( params[:user_id] )
+      @profile = @user.build_profile(profile_params)
+      if @profile.save
+        flash[:success] = "Profile Updated!"
+        redirect_to user_path( params[:user_id] )
+      else
+        render action: :new
+      end
     end
-  end
-  private
-    def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    def update
+      @user = User.find( params[:user_id] )
+      @profile = @user.profile
+      if @profile.update_attributes(profile_params)
+        flash[:success] = "Profile Updated!"
+        redirect_to user_path( params[:user_id] )
+      else
+        render action: :edit
+      end
     end
+    def edit
+      @user = User.find( params[:user_id] )
+      @profile = @user.profile
+    end
+    private
+      def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      end
 end
